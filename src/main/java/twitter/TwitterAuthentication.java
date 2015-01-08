a28c968d8e6b29a67e4f8e6a135958fd59d82911
Øyvind <oyvind.midtbo@bekk.no>
2015-01-08 13:41:32 +0100

    Lagt til properties for Twitter-autentisering.

--- src/main/java/twitter/TwitterAuthentication.java
+++ src/main/java/twitter/TwitterAuthentication.java
@@ -1,8 +1,41 @@
 package twitter;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class TwitterAuthentication {
-    public static String consumerKey = "";
-    public static String consumerSecret = "";
-    public static String accessToken = "";
-    public static String accessTokenSecret = "";
+    
+    private Properties getProperties() {
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("twitter-authentication.properties");
+
+        if (inputStream != null) {
+            try {
+                properties.load(inputStream);
+            } catch (IOException e) {
+                System.out.println("Filen twitter-authentication.properties ble ikke funnet på classpath.");
+                e.printStackTrace();
+            }
+        }
+        
+        return properties;
+    }
+    
+    public String getConsumerKey() throws IOException {
+        return getProperties().getProperty("consumerKey");
+    }
+
+    public String getConsumerSecret() throws IOException {
+        return getProperties().getProperty("consumerSecret");
+    }
+
+    public String getAccessToken() throws IOException {
+        return getProperties().getProperty("accessToken");
+    }
+
+    public String getAccessTokenSecret() throws IOException {
+        return getProperties().getProperty("accessTokenSecret");
+    }
 }
