2566625f5f3569e0ce73522ee083dbe44028b70c
Brynjulv Brynjulvsen <brynjulv.brynjulvsen@bekk.no>
2015-01-08 17:01:52 +0100

    Mulighet for å dytte tweet-objekter til neo4j

--- src/main/java/db/Provider.java
+++ src/main/java/db/Provider.java
@@ -1,13 +1,24 @@
 package db;
 
-import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.cypher.ExecutionEngine;
+import org.neo4j.cypher.ExecutionResult;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.kernel.impl.util.StringLogger;
+import scala.collection.Iterator;
+import twitter.Tweet;
+
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class Provider {
     GraphDatabaseService graphDb;
+    private static enum RelTypes implements RelationshipType
+    {
+        REPLY_TO
+    }
 
     private GraphDatabaseService createDatabase(){
         return new GraphDatabaseFactory().newEmbeddedDatabase(createTempDatabaseDir().getAbsolutePath());
@@ -43,5 +54,101 @@ public class Provider {
         }
         return graphDb;
     }
+
+    public boolean createTweet(Tweet tweet){
+        GraphDatabaseService db = getDatabase();
+        try ( Transaction tx = db.beginTx(); )
+        {
+            Node tweetNode = db.createNode();
+            tweetNode.setProperty( "text", tweet.getText() );
+            tweetNode.setProperty( "userId", tweet.getUserId());
+            tweetNode.setProperty( "tweetId", tweet.getTweetId());
+            if(tweet.getInReplyToStatusId() != null && tweet.getInReplyToStatusId().length() > 0){
+                Node replyToNode = getTweet(tweet.getInReplyToStatusId());
+                if(replyToNode != null) {
+                    tweetNode.createRelationshipTo(replyToNode, RelTypes.REPLY_TO);
+                }
+            }
+            Label tweetLabel = DynamicLabel.label("Tweet");
+            tweetNode.addLabel(tweetLabel);
+            tx.success();
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public Node getTweet(String tweetId){
+        ExecutionEngine engine = new ExecutionEngine( getDatabase(), StringLogger.DEV_NULL );
+        GraphDatabaseService db = getDatabase();
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("tweetId",tweetId);
+        String query = "MATCH (node:Tweet {tweetId: {tweetId} }) RETURN node";
+        ExecutionResult result = engine.execute(query, params);
+        Iterator<Node> nodes = result.columnAs("node");
+        if (nodes.hasNext()) {
+            Node tweet = nodes.next();
+            return tweet;
+        } else {
+            return null;
+        }
+
+    }
+
+    public Set<Node> getReplies(Node node){
+        Set<Node> replies = new HashSet<Node>();
+        ExecutionEngine engine = new ExecutionEngine( getDatabase(), StringLogger.DEV_NULL );
+        GraphDatabaseService db = getDatabase();
+        try ( Transaction tx = db.beginTx(); ) {
+            for (Relationship relationship : node.getRelationships(RelTypes.REPLY_TO, Direction.INCOMING)) {
+                replies.add(relationship.getStartNode());
+            }
+        }
+        return replies;
+    }
+
+
+/*
+    public static void main(String[] args){
+        Provider provider = new Provider();
+        GraphDatabaseService db = provider.getDatabase();
+        System.out.println(provider.createTweet(
+                new Tweet()
+                .setTweetId("1")
+                .setText("Tweeet tweeeeet")
+                .setUserId("1")
+        ));
+
+        System.out.println(provider.createTweet(
+                new Tweet()
+                        .setTweetId("2")
+                        .setText("Twooot tweeeeet")
+                        .setUserId("1")
+        ));
+
+        System.out.println(provider.createTweet(
+                new Tweet()
+                        .setTweetId("3")
+                        .setText("Tweeet tweeeeet")
+                        .setUserId("2")
+                        .setInReplyToStatusId("1")
+        ));
+
+        Node tweet = provider.getTweet("1");
+        try(Transaction tx = db.beginTx()){
+            for (String propertyKey : tweet.getPropertyKeys()) {
+                System.out.println("\t" + propertyKey + " : " + tweet.getProperty(propertyKey));
+            }
+            tx.success();
+        }
+        System.out.println(provider.getReplies(provider.getTweet("1")).size());
+    }
+*/
+
+
+
 
 }
