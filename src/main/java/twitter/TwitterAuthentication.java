b211ac104047945dd9fe58a092bdaf55f92bfeff
Øyvind <oyvind.midtbo@bekk.no>
2015-01-08 11:54:53 +0100

    Commit bare for å få opp mappestrukturen

---
+++ src/main/java/twitter/TwitterAuthentication.java
@@ -0,0 +1,8 @@
+package twitter;
+
+public class TwitterAuthentication {
+    public static String consumerKey = "";
+    public static String consumerSecret = "";
+    public static String accessToken = "";
+    public static String accessTokenSecret = "";
+}
